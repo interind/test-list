9f1814f85352118542dd93572b40bdd3901a9be9
Aleksandr <interind@yandex.ru>
2022-03-21 12:51:29 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
+import { useState, useEffect } from 'react';
+import { Badge, Button } from 'react-bootstrap';
+// import { url } from './config/config.js';
+import info from './config/res.json';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const date = Date.now();
+  const [listCurrency, setListCurrency] = useState(['dollars', 'euro', 'rub']);
+  const getListCurrency = ({ Valute: value }) => {
+    const arr = Object.entries(value);
+    console.log(arr);
+    setListCurrency(arr)
+  };
+
+  function formatDate(date) {
+  return new Date(date).toLocaleString('ru', {
+    day:   '2-digit',
+    month: '2-digit',
+    year:  '2-digit'
+  });
+}
+
+  useEffect(() => {
+    // fetch(url)
+    //   .then(res => res.ok ? res.json() : new Error('Error connect'))
+    //   .then(result => getListCurrency(result))
+    //   .catch(err => console.error(err))
+    getListCurrency(JSON.parse(JSON.stringify(info)))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{formatDate(date)}</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="outline-primary">Primary</Button>{' '}
+        <ul className="list-group">
+          {listCurrency.length ?
+            listCurrency.map(([ico, value], id) =>
+              <li className="list-group-item" key={id}>
+                {value.Name}
+                <Badge bg="primary">{ico}</Badge>
+              </li>
+            )
+            : <li>список пуст</li>
+          }
+        </ul>
       </header>
     </div>
   );
---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: 'https://www.cbr-xml-daily.ru/archive/2022/02/11/daily_json.js',
+};
